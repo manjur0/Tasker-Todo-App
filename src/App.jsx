a373fd25522daf2e89841538d77c6f00970d6778
manjur0 <sfmanjur21@gmail.com>
2024-01-22 09:23:10 +0600

    header footer and hero added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import "./App.css";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import Hero from "./Components/Hero";
 
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      <Header />
+      <Hero />
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer className="py-6 md:py-8">
+      <div className="container mx-auto">
+        <p className="text-center text-xs text-white lg:text-sm">
+          Copyright ©2024 | All rights reserved by Manjur Jubu
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
