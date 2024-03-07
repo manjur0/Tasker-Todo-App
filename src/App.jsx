1fccc7a8a06dac837d2ed82a898182e144e67646
Manjur00 <sfmanjur21@gmail.com>
2024-03-07 22:06:46 +0600

    Task Bord HTML Structure added

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import Hero from "./Components/Hero";
+import TaskBord from "./Components/Task/TaskBord";
 
 function App() {
   return (
     <div>
       <Header />
       <Hero />
+      <TaskBord />
       <Footer />
     </div>
   );
--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,8 +1,8 @@
 import Frame from "../assets/frame.png";
 const Hero = () => {
   return (
-    <section className="pb-[114px] pt-20 md:mt-[100px] text-white">
-      <div className="container lg:px-20">
+    <section className="pb-[114px]  pt-20 md:mt-[100px] text-white">
+      <div className=" lg:px-20">
         <div className="grid items-center gap-6 md:grid-cols-2">
           <div className="flex justify-center md:order-2">
             <img
