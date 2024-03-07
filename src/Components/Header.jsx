b854789adc5a4aad1e5080444c81d51be6c77371
Manjur00 <sfmanjur21@gmail.com>
2024-03-07 22:57:12 +0600

    Component is divided

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,7 @@
 import Logo from "../assets/lws-logo-en.svg";
 const Header = () => {
   return (
-    <nav className="py-6 md:py-8 fixed top-0 w-full !bg-[#191D26] z-50">
+    <nav className="py-6 md:py-8  top-0 w-full mx-auto !bg-[#191D26] z-50 shadow-md">
       <div className="container mx-auto flex items-center justify-between gap-x-6">
         <a href="/">
           <img className="h-[45px]" src={Logo} alt="Tasker Logo" />
