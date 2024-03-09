f86eef88edfd9cb8e73647e2847779883d16536f
Manjur00 <sfmanjur21@gmail.com>
2024-03-09 09:48:03 +0600

    Items Added dynamically

--- src/Components/Task/TaskBord.jsx
+++ src/Components/Task/TaskBord.jsx
@@ -1,8 +1,19 @@
+import { useState } from "react";
 import SearchTask from "./SearchTask";
 import TaskAction from "./TaskAction";
 import TaskList from "./TaskList";
 
 const TaskBord = () => {
+  const defaultTask = {
+  "id": crypto.randomUUID(),
+   "title": "Learn React Js",
+   "description":
+      "These symbols play various roles in programming languages and are used for tasks such as defining blocks of code, indicating the start and end of arrays or objects,",
+    "tags": ["web", "react", "tailwind"],
+    "property": "High",
+    "isFaverate": true,
+  };
+  const [tasks, setTasks] = useState([defaultTask]);
   return (
     <section className="mb-20" id="tasks">
       <div className="container">
@@ -12,7 +23,7 @@ const TaskBord = () => {
 
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
           <TaskAction />
-          <TaskList />
+          <TaskList tasks={tasks} setTasks={setTasks} />
         </div>
       </div>
     </section>
